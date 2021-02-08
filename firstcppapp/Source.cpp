a5ba7ed8943482a2222c86df1737640eaea589fb
ryan <ryanflynn44@gmail.com>
2021-02-08 10:34:57 -0500

    Learned if else else if

--- firstcppapp/Source.cpp
+++ firstcppapp/Source.cpp
@@ -2,21 +2,18 @@
 using namespace std;
 
 int main() {
-	char myCharacter;
-	myCharacter = 'y';
+	int a(1);
+	int b = 13;
 
-	int myNum;
-	myNum = 13;
-
-	cout << myCharacter << endl;
-	cout << myNum << endl;
-
-	myCharacter = 'n';
-	myNum = 14;
-
-	cout << myCharacter << endl;
-	cout << myNum << endl;
-
+	if (a < b) {
+		cout << "a is less than b" << endl;
+	}
+	else if (b < a) {
+		cout << "b is less than a" << endl;
+	}
+	else {
+		cout << "a is equal to b" << endl;
+	}
 
 	system("pause");
 
