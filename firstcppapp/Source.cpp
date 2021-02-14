26438e4f2ccbbbb504bcb512c45122fabd03430a
ryan <ryanflynn44@gmail.com>
2021-02-14 15:32:06 -0500

    enum blog

--- firstcppapp/Source.cpp
+++ firstcppapp/Source.cpp
@@ -1,42 +1,24 @@
 #include <iostream>
 using namespace std;
 
-void welcome();
-char getYesOrNo();
-void printResponse(char response);
-
-void askYesOrNo();
+enum SubmitButtonStatus {
+	SB_Not_Clicked,
+	SB_Clicked
+};
 
 int main() {
-	
-	askYesOrNo();
 
+	SubmitButtonStatus button_status;
+	button_status = SB_Clicked;
+	
+	if(button_status == SB_Clicked){
+		cout << "Submit button has been clicked!" << endl;
+	}
+	else if (button_status == SB_Not_Clicked) {
+		cout << "Submit button has not been clicked yet" << endl;
+	}
+	
 	system("pause");
-}
-
-void welcome() {
-	cout << "Welcome!" << endl;
-}
-
-char getYesOrNo() {
-
-	cout << "Enter Yes or No y/n" << endl; 
-	
-	char response; 
-	cin >> response;
-
-	return response;
 
 }
 
-void printResponse(char response) {
-
-	cout << "Your answer was: " << response << endl;
-}
-
-void askYesOrNo() {
-	welcome();
-
-	char answer = getYesOrNo();
-	printResponse(answer);
-}
