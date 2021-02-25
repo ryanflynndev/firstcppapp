950f6628c2f3f24a552e00dc8a7cb2d7a9bc368d
ryan <ryanflynn44@gmail.com>
2021-02-24 19:25:28 -0500

    Learning variable references

--- firstcppapp/Source.cpp
+++ firstcppapp/Source.cpp
@@ -1,34 +1,17 @@
 #include <iostream>
 using namespace std;
 
-struct Player
-{
-	int Level;
-	float Health;
-	float Damage;
-	float Stamina;
-
-	void TakeDamage(float dmg) {
-		Health -= dmg;
-	}
-
-	int GetLevel()
-	{
-		if (Level > 10) {
-			cout << "Level is over 10! \n";
-		}
-		return Level;
-	}
-};
-
-
 
 int main() 
 {
-	Player ryan = { 11, 100.f, 10.f, 50.f };
+	string myStr = "Druid";
+	string& myRef = myStr;
 
+	myRef += " Mechanics";
 
-	cout << "Ryan's Level = " << ryan.GetLevel() << endl;
+	cout << myStr << endl;
+	cout << myRef << endl;
+
 	
 	system("pause");
 
